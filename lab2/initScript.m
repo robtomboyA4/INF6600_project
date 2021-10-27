6f2cd9fdea5dd2f051e6d98ee10e77b7119ae8a6
robtomboyA4 <92056172+robtomboyA4@users.noreply.github.com>
2021-10-27 13:41:34 -0400

    Add files via upload

--- lab2/initScript.m
+++ lab2/initScript.m
@@ -5,7 +5,7 @@ clc
 run('../truetime-2.0/init_truetime.m')  %pour initialiser les variables truetime
 
 
-mex -setup:'C:\Program Files\MATLAB\R2021a\bin\win64\mexopts\mingw64_g++.xml' C++
+mex -setup:'C:\Program Files\MATLAB\R2020a\bin\win64\mexopts\mingw64_g++.xml' C++
 
 %make_truetime          %pour la compilation de la librairie truetime
 ttmex ctrlSM.cpp     %pour la compilation du code du fichier c++
