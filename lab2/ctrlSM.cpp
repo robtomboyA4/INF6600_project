aa6ef20491e267a2db967564dc423f6adbd0e5a7
robtomboyA4 <92056172+robtomboyA4@users.noreply.github.com>
2021-10-27 13:42:10 -0400

    Add files via upload

--- lab2/ctrlSM.cpp
+++ lab2/ctrlSM.cpp
@@ -3,109 +3,104 @@
 #include "mex.h"
 
 #include <time.h>
-#include <random>
 #include <cmath>
+//#include <random>
 
-#define RAND_MIN -1000
-#define RAND_MAX  1000
+#define CTRL_TASK_PERIOD 0.1
+#define VIT_OPE 5
 
-#define CTRL_TASK_PERIOD 60
+#define P_HIGH 0
+#define P_LOW -1000
+#define LAMBDA 1000
 
 #define PI 3.141592
 
-// System Outputs
-// Data structure used for the task data
-#define CONS_ALPHA
-#define CONS_THETA
-#define CONS_VITESSE
+//System Outputs
+#define CONS_ALPHA 1
+#define CONS_THETA 2
+#define CONS_VITESSE 3
 
-
-// System Inputs
+//System Inputs
 #define BATTERY_LEVEL 1
-#define POS_X 2
-#define POS_Y 3
-#define POS_Z 4
-#define ALPHA 5
-#define THETA 6
-#define VITESSE 7
-#define DISTANCE 8
+#define OBS_FAITES 2
+#define POS_X 3
+#define POS_Y 4
+#define POS_Z 5
+#define ALPHA 6
+#define THETA 7
+#define VITESSE 8
+#define DISTANCE 9
+
+//System Triggers
+#define TRIGGER_ALARM_BAT 1
+#define TRIGGER_DATA 2
 
 // Data structure used for the task data
 
 struct TaskData {
-	double battery_lvl;
-    
+    double battery_lvl;
+    double obs_faites;
     double curr_x;
     double curr_y;
     double curr_z;
-    
-    double dest_x; 
-    double dest_y;
-    
-    double curr_alpha; 
-    
+    double curr_alpha;
     double curr_theta;
     double curr_vit;
     double distance;
-    
     double d_alpha;
     double d_theta;
     double d_vit;
 };
 
-struct Mission {
-    double x;
-    double y;
-}
-
-Mission* get_new_mission(){
-    Mission *m= new Mission();
-    std::uniform_real_distribution<double> unif(RAND_MIN,RAND_MAX);
-    std::default_random_engine re;
-    m->x = unif(re);
-    m->y = unif(re);
-    return m
-}
 
 double navig_function(int segment, void* data) {
     TaskData *d = static_cast<TaskData*>(data);
     
-    switch (segment) {
-        case 1:
-            // read inputs from operative system
-            d->battery_lvl = ttAnalogIn(BATTERY_LEVEL);
-            d->curr_x = ttAnalogIn(POS_X);
-            d->curr_y= ttAnalogIn(POS_Y);
-            d->curr_z= ttAnalogIn(POS_Z);
-            d->curr_alpha= ttAnalogIn(ALPHA);
-            d->curr_theta= ttAnalogIn(THETA);
-            d->curr_vit= ttAnalogIn(VITESSE);
-            d->distance= ttAnalogIn(DISTANCE);
-            return 5;
-        case 2:
-            if(d->d_alpha != 0) // condition i was thinking of adding a boolean value to see whether or not we need to start a new mission
-            {
-                mexPrintf("Setting Alpha\n");
-                d->curr_alpha += d->d_alpha;
-                d->d_alpha = 0;
-            }
-            else if(true) // some condition
-            {
-                mexPrintf("Setting d_alpha\n");
-                d->d_alpha = atan((dest_y- curr_y) / (dest_x- curr_x)); // if we want to make it deg => *180/PI
-                if (dest_x- curr_x< 0) // gotta double check this one
-                    d->d_alpha= - d->d_alpha;
-            }
-            return 1;
-        default:
-            // send ouputs to operative system
-            //ttAnalogOut(VALVE_CTRL, d->open_valve);
-            
-            // We gotta AnalogOut the dAlpha
-            
-            return FINISHED;
-    }
+        switch (segment) {
+            case 1:
+                d->battery_lvl = ttAnalogIn(BATTERY_LEVEL);
+                d->obs_faites = ttAnalogIn(OBS_FAITES);
+                d->curr_x = ttAnalogIn(POS_X);
+                d->curr_y= ttAnalogIn(POS_Y);
+                d->curr_z= ttAnalogIn(POS_Z);
+                d->curr_alpha= ttAnalogIn(ALPHA);
+                d->curr_theta= ttAnalogIn(THETA);
+                d->curr_vit= ttAnalogIn(VITESSE);
+                d->distance= ttAnalogIn(DISTANCE);
+                return 0.000009;
+            case 2:
+                if(d->curr_x != LAMBDA || d->curr_z != (P_LOW-P_HIGH))
+                {
+                    mexPrintf("Correct Alpha\n");
+                    double alpha = atan2((P_LOW-P_HIGH - d->curr_z),LAMBDA - d->curr_x)*180/PI;
+                    d->d_alpha = (alpha - d->curr_alpha);
+                    //d->d_alpha = alpha;
+                    d->d_theta = 0;
+                    if(d->curr_vit != 5) {
+                        d->d_vit = 5 - d->curr_vit;
+                    }else {
+                       d->d_vit = 0;
+                    }
+                } else
+                {
+                    d->d_alpha = 0;
+                    d->d_theta = 0;
+                    d->d_vit = -5;
+                }
+                return 0.000008;
+            default:
+                // send ouputs to operative system
+                ttAnalogOut(CONS_ALPHA, d->d_alpha);
+                ttAnalogOut(CONS_THETA, d->d_theta);
+                ttAnalogOut(CONS_VITESSE, d->d_vit);
+                
+                return FINISHED;
+        }
+    
+    
 }
+
+
 
 
 // Kernel init function    
@@ -114,14 +109,14 @@ void init(){
     TaskData *data = new TaskData;
     ttSetUserData(data);
     memset( data, 0, sizeof(TaskData) );
-
+    
     ttInitKernel(prioFP);
-
+    
     mexPrintf("Simulation started\n");
-
-    ttCreatePeriodicTask("nav", 0.0, CTRL_TASK_PERIOD,
-                        navig_function, data);
+    
+    ttCreatePeriodicTask("nav", 0.0, CTRL_TASK_PERIOD, navig_function, data);
     ttSetPriority(1, "nav");
+    
 }
 
 // Kernel cleanup function
@@ -129,6 +124,6 @@ void cleanup() {
     // Free the allocated memory
     TaskData *d = static_cast<TaskData*>(ttGetUserData());
     delete d;
-
+    
     mexPrintf("Simulation ended\n");
 }
